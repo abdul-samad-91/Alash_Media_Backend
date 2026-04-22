03a419e3340e17c5aa673e6aaa8c5852ea51e0a1
Abdul Samad <as03489822@gmail.com>
2026-04-22 22:20:18 +0500

    update cors origin

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ const app = express();
 await connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
